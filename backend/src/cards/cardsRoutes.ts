da54e0fbf11936fdd97e2a51ce73e8fd4965a4a7
javonjax <javonj461@hotmail.com>
2025-05-05 20:53:33 -0400

    Added API endpoint for searching cards by oracle text. This feature uses https://api.magicthegathering.io because scryfall does not support searching by card text.

--- backend/src/cards/cardsRoutes.ts
+++ backend/src/cards/cardsRoutes.ts
@@ -8,6 +8,7 @@ dotenv.config();
 */
 const SCRYFALL_CARDS_RANDOM_URL = process.env.SCRYFALL_CARDS_RANDOM_URL;
 const SCRYFALL_CARDS_SEARCH_URL = process.env.SCRYFALL_CARDS_SEARCH_URL;
+const MTG_IO_CARDS_SEARCH_URL = process.env.MTG_IO_CARDS_SEARCH_URL;
 const SCRYFALL_SETS_URL = process.env.SCRYFALL_SETS_URL;
 
 const router: Router = express.Router();
@@ -30,6 +31,9 @@ router.get('/cards/random', async (req: Request, res: Response) => {
 
 /*
   GET cards using a text search string.
+  NOTE: The scryfall API does not support querying by oracle text
+        but it does provide more options for images, links for purchasing cards,
+        card keywords, etc. Use the searchbycardtext endpoint for searching by oracle text.
 */
 router.get('/cards/search', async (req: Request, res: Response) => {
   try {
@@ -41,6 +45,29 @@ router.get('/cards/search', async (req: Request, res: Response) => {
     const apiResponse: globalThis.Response = await fetch(
       `${SCRYFALL_CARDS_SEARCH_URL}?${queryParams}`
     );
+    const responseData: unknown = await apiResponse.json();
+    res.send(responseData);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/*
+  GET cards using a text search string. 
+  This endpoint is specifically for searching for cards by their oracle text.
+*/
+router.get('/cards/searchbycardtext', async (req: Request, res: Response) => {
+  try {
+    if (typeof MTG_IO_CARDS_SEARCH_URL !== 'string') {
+      throw new TypeError('MTG IO search API url not set.');
+    }
+
+    const queryParams: string = convertQueryToURLSearchParams(req).toString();
+    console.log(`${MTG_IO_CARDS_SEARCH_URL}?${queryParams}`);
+    const apiResponse: globalThis.Response = await fetch(
+      `${MTG_IO_CARDS_SEARCH_URL}?${queryParams}`
+    );
+
     const responseData: unknown = await apiResponse.json();
     res.send(responseData);
   } catch (error) {
